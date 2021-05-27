da1cf23eab98ba9f1a6aaa8c92ce577af9427793
LukeStMarie <lukestmarie@gmail.com>
2021-05-27 15:54:10 -0400

    Add files via upload

--- layerID_GMM.py
+++ layerID_GMM.py
@@ -92,8 +92,9 @@ def preprocess(img_file, save_dir="none"):
     # Unflattens the mask to a 2D image
     img_gmm_mask = gmm_mask.reshape(dims[0],dims[1])
     
-    plt.figure()
-    plt.imshow(img_gmm_mask)
+    # Show background/foreground mask
+    #plt.figure()
+    #plt.imshow(img_gmm_mask)
    
     
 
@@ -125,8 +126,9 @@ def preprocess(img_file, save_dir="none"):
                           (G-Gfit+1).reshape(y_dim,x_dim)/2,
                           (B-Bfit+1).reshape(y_dim,x_dim)/2])
     
-    plt.figure()
-    plt.imshow(img_poly)
+    # Show subtracted image
+    # plt.figure()
+    # plt.imshow(img_poly)
     
     
     # Flattens the background-subtracted image
@@ -264,14 +266,14 @@ def training(img_dir, n_clusters, out_file):
     return
 
 
-def testing(img_file, n_clusters, master_cat_file):
+def testing(img_dir, n_clusters, master_cat_file):
     """
     Identify thickness of flake "img_file" using "master_cat_file"
 
     Parameters
     ----------
-    img_file : str
-        Location of image file. (i.e., "...\\RSGR003\\All\\4A1.jpg")
+    img_dir : str
+        Location of folder of image files.
     n_clusters : int
         Number of layers to fit up to. Determine based on how many layers
         "master_cat_file" was trained to.
@@ -306,52 +308,50 @@ def testing(img_file, n_clusters, master_cat_file):
     
    
     
-    ## Original image
-    img = cv2.cvtColor(cv2.imread(img_file), cv2.COLOR_BGR2RGB)
-    img_array = np.asarray(img)
-    dims = img_array.shape
-    
-    ## Import and pre-processing
-    ## Image import
-    img_array2D = preprocess(img_file)
-    
-    # Apply GMM to image
-    gmm_pred = gmm.predict(img_array2D)
-    
-    ## Set pixels that didn't fit into any cluster to the highest value   
-    score = gmm.score_samples(img_array2D)    
-    gmm_pred = np.where(score<1,n_clusters,gmm_pred)
-    
-
-    # Unflatten image
-    img_gmm_pred = gmm_pred.reshape(dims[0],dims[1])
-    
-    
-    ## Plot layer map and color maps of original, pre-processed, and final images
-    plt.figure()
-    plt.imshow(img_gmm_pred)    
-
-    
-    # R = img_array2D[:,0].flatten()
-    # G = img_array2D[:,1].flatten()
-    # B = img_array2D[:,2].flatten()
-
-    
-    # ## Scatter plot of data points, of pre-processed image.
-    # fig1 = plt.figure() 
-    # ax1 = fig1.add_subplot(111, projection='3d')
-    # ax1.scatter(B, G, R, s=0.5, c=img_array2D, marker=',')
-    # ax1.set_xlabel('Blue')
-    # ax1.set_ylabel('Green')
-    # ax1.set_zlabel('Red')
-    
-    # ## Scatter plot of data points, after clustering
-    # fig2 = plt.figure() 
-    # ax2 = fig2.add_subplot(111, projection='3d')
-    # ax2.scatter(B, G, R, c=gmm_pred, s=0.5, marker=',')
-    # ax2.set_xlabel('Blue')
-    # ax2.set_ylabel('Green')
-    # ax2.set_zlabel('Red')
+    for filename in os.listdir(img_dir):
+        if filename.endswith(".jpg"):
+            img_file = os.path.join(img_dir, filename)
+            ## Original image
+            img = cv2.cvtColor(cv2.imread(img_file), cv2.COLOR_BGR2RGB)
+            img_array = np.asarray(img)
+            dims = img_array.shape
+            
+            ## Import and pre-processing
+            ## Image import
+            img_array2D = preprocess(img_file)
+            
+            # Apply GMM to image
+            gmm_pred = gmm.predict(img_array2D)
+            
+            ## Set pixels that didn't fit into any cluster to the highest value   
+            score = gmm.score_samples(img_array2D)    
+            gmm_pred = np.where(score<1,n_clusters,gmm_pred)
+            
+            # Unflatten image
+            img_gmm_pred = gmm_pred.reshape(dims[0],dims[1])
+            
+            ## Plot layer map and color maps of original, pre-processed, and final images
+            plt.figure()
+            plt.imshow(img_gmm_pred)    
+        
+            # Can use this to plot "pixel clouds" of the images
+            # R = img_array2D[:,0].flatten()
+            # G = img_array2D[:,1].flatten()
+            # B = img_array2D[:,2].flatten()            
+            # ## Scatter plot of data points, of pre-processed image.
+            # fig1 = plt.figure() 
+            # ax1 = fig1.add_subplot(111, projection='3d')
+            # ax1.scatter(B, G, R, s=0.5, c=img_array2D, marker=',')
+            # ax1.set_xlabel('Blue')
+            # ax1.set_ylabel('Green')
+            # ax1.set_zlabel('Red')
+            # ## Scatter plot of data points, after clustering
+            # fig2 = plt.figure() 
+            # ax2 = fig2.add_subplot(111, projection='3d')
+            # ax2.scatter(B, G, R, c=gmm_pred, s=0.5, marker=',')
+            # ax2.set_xlabel('Blue')
+            # ax2.set_ylabel('Green')
+            # ax2.set_zlabel('Red')
     
 
     
@@ -366,25 +366,23 @@ def testing(img_file, n_clusters, master_cat_file):
 """ Example usage """
 
 # This runs training on images in a folder, creating a catalog that can
-# discern between 5 different layer thicknesses (including bare substrate)
+# discern between 4 different layer thicknesses (including bare substrate)
 
-args1 = {'img_dir': "CropImageSetB",
-        'n_clusters': 5,
-        'out_file': 'CropImageSetB/master_catalog_5.npz'}
+# args1 = {'img_dir': "CropImageSetB",
+#         'n_clusters': 4,
+#         'out_file': 'CropImageSetC/master_catalog_4.npz'}
 
-training(**args1)
+# training(**args1)
 
 
 
 # This uses the catalog created above to identify layers in an image.
 
-# img_file = "ImageSetB/Chip49Flake08.jpg"
+args2 = {'img_dir': 'CropImageSetB',
+        'n_clusters': 5,
+        'master_cat_file': 'CropImageSetB/master_catalog_5.npz'}
 
-# args2 = {'img_file': img_file,
-#         'n_clusters': 5,
-#         'master_cat_file': 'CropImageSetB/master_catalog_5.npz'}
-
-# testing(**args2)
+testing(**args2)
 
 
 
